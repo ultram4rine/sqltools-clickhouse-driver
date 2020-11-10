db5e67a7508677545c49c5b08f8c788907bf86d2
ultram4rine <ultramarine730@gmail.com>
2020-11-10 15:59:36 +0400

    improved search queries

--- src/ls/queries.ts
+++ src/ls/queries.ts
@@ -48,22 +48,36 @@ ORDER BY t.name
 `;
 
 const searchTables: IBaseQueries["searchTables"] = queryFactory`
-SELECT name AS label,
-  t.engine AS type
+SELECT t.name AS label,
+  t.database AS database,
+  '${ContextValue.TABLE}' AS type
 FROM system.tables AS t
-WHERE ${(p) =>
-  p.search ? `WHERE LOWER(name) LIKE '%${p.search.toLowerCase()}%'` : ""}
-  AND database == '${(p) => p.database}'
-ORDER BY name
+WHERE t.database NOT IN ('_temporary_and_external_tables', 'default', 'system')
+  ${(p) => (p.search ? `AND t.name ILIKE '%${p.search}%'` : "")}
+ORDER BY t.name
 `;
 const searchColumns: IBaseQueries["searchColumns"] = queryFactory`
 SELECT c.name AS label,
   c.table AS table,
+  c.database AS database,
   c.type AS dataType,
   '${ContextValue.COLUMN}' as type
 FROM system.columns AS c
-WHERE database = ${(p) => p.database} AND table = ${(p) =>
-  p.table.label || p.table}
+WHERE c.database NOT IN ('_temporary_and_external_tables', 'default', 'system')
+  ${(p) =>
+    p.tables.filter((t) => !!t.label).length
+      ? `AND LOWER(c.table) IN (${p.tables
+          .filter((t) => !!t.label)
+          .map((t) => `'${t.label}'`.toLowerCase())
+          .join(", ")})`
+      : ""}
+${(p) =>
+  p.search
+    ? `AND (
+    (C.TABLE_NAME || '.' || C.COLUMN_NAME) ILIKE '%${p.search}%'
+    OR C.COLUMN_NAME ILIKE '%${p.search}%'
+  )`
+    : ""}
 ORDER BY c.name ASC
 LIMIT ${(p) => p.limit || 100}
 `;
